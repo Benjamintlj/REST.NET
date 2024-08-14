2f1ca5da7bd18b7dc13d474dfe32afcc3e0b9d72
n0928518 <n0928518@my.ntu.ac.uk>
2024-08-14 11:17:45 +0100

    seeded data

--- REST.NET/Data/ApplicationDbContext.cs
+++ REST.NET/Data/ApplicationDbContext.cs
@@ -6,12 +6,15 @@ namespace REST.NET.Data;
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
     public DbSet<Game> Games => Set<Game>();
-
+    public DbSet<Genre> Genres => Set<Genre>();
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Game>().Property(g => g.Price).HasColumnType("decimal(18, 2)");
+
+        modelBuilder.Entity<Genre>().HasData(
+            new {Id = 3, Name = "fun"},
+            new {Id = 4, Name = "bad"}
+        );
     }
-    
-    public DbSet<Genre> Genres => Set<Genre>();
-    
 }
--- REST.NET/obj/Debug/net8.0/REST.NET.AssemblyInfo.cs
+++ REST.NET/obj/Debug/net8.0/REST.NET.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("REST.NET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+026cbeacb12851185931f0823759d6d9fcaee66d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a696a928417532b73b175bef91864736dee21e72")]
 [assembly: System.Reflection.AssemblyProductAttribute("REST.NET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("REST.NET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
